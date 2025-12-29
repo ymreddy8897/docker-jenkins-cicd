214bc11ec3643a125cf8d012141948c732a8e8fd
ymreddy8897 <ymreddy8897@gmail.com>
2025-12-29 15:05:43 +0530

    first commit

---
+++ src/main/java/com/comviva/DockerJenkinCicdApplication.java
@@ -0,0 +1,22 @@
+package com.comviva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerJenkinCicdApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinCicdApplication.class, args);
+	}
+
+}
